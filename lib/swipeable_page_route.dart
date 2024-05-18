5761a42cf7d5d47331a1f39cf2698f7e68223e07
0xManjeet <0xManjeet@gmail.com>
2024-05-18 07:54:33 +0530

    -

--- lib/swipeable_page_route.dart
+++ lib/swipeable_page_route.dart
@@ -1,8 +1,2 @@
 export 'src/app_bar/app_bar.dart' show MorphingAppBar;
 export 'src/app_bar/sliver_app_bar.dart' show MorphingSliverAppBar;
-export 'src/page_route.dart'
-    show
-        BuildContextSwipeablePageRoute,
-        SwipeablePage,
-        SwipeablePageRoute,
-        SwipeablePageTransitionsBuilder;
